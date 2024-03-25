deccb4b8bb6d80253828d1cd59433e3e8587f7e3
anu03k <Kafleanuja1@gmail.com>
2024-03-25 19:49:39 +0545

    DB connect

---
+++ server/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const port = process.env.PORT || 3000; // Use port 3000 by default
+const DB = 'mongodb+srv://kafleanuja1:FitPulse123@cluster0.eqitq8f.mongodb.net/UserInfo?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(DB).then
+
+
+
+//middleware is fucntion that has acess to req obj and res obj and next function in application's req-res cycle
+const middleware = (req, res, next) => {
+    // response paxi direct arko start so next
+    console.log('This is middleware')
+    next();
+    // n Express.js, next() is a function that is passed to middleware functions and route handlers. It is used to pass control to the next middleware function or route handler in the stack.
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// Define routes
+app.get('/', (req, res) => {
+    console.log('main page')
+    res.send('Hello, World! This is your Express server.');
+});
+
+
+app.get('/dashboard', middleware, (req, res) => {
+    res.send('Hello, World! This is your Dashboard server.');
+});
+app.get('/signup', (req, res) => {
+    res.send('Hello, World! Signup server.');
+});
+
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import SignIn from './components/SignIn'
+
+import './App.css'
+
+function App() {
+  return ( 
+  <>
+  <SignIn />
+   sss
+    </>
+    )
+  
+}
+
+export default App
