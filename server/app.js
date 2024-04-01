8af91b6f090525db4f8b5993f942694718ba7b7c
anu03k <Kafleanuja1@gmail.com>
2024-04-01 08:11:03 +0545

    connection

--- server/app.js
+++ server/app.js
@@ -1,11 +1,19 @@
+const e = require('express');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const port = process.env.PORT || 3000; // Use port 3000 by default
 const DB = 'mongodb+srv://kafleanuja1:FitPulse123@cluster0.eqitq8f.mongodb.net/UserInfo?retryWrites=true&w=majority&appName=Cluster0'
-mongoose.connect(DB).then
 
+mongoose.connect(DB, {
 
+    // The second argument is an options object with various options for the connection.
+}).then(() => {
+    // .then() is used to handle the result of promise after it had been resolved or rejected - promise.then(onfullfilled  onrejected)
+    console.log(`connection established with db`)
+}).catch((e) => console.log('error in connection'));
+
+// .then(() => {...})
 
 //middleware is fucntion that has acess to req obj and res obj and next function in application's req-res cycle
 const middleware = (req, res, next) => {
